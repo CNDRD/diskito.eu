37c54d8ad399fb12dd90206627571bb7700a9bb5
Michal Mikulec <20823071+CNDRD@users.noreply.github.com>
2022-02-21 13:46:01 +0100

    simplified and reworked how we show ntfs

--- public/resources/js/nft.js
+++ public/resources/js/nft.js
@@ -1,37 +1,55 @@
-let ids = new URLSearchParams(window.location.search).get('ids');
+let id = new URLSearchParams(window.location.search).get('id');
 
-if (ids === null) {
+function wrongOrMissingIdPage() {
+
   $("#page_header").text("👀👀 🚀🚀🚀");
   $("#cost").text("No NFTs here bruh..");
+
 };
 
+if (id === null) {
 
-ids = ids.split(',');
+  wrongOrMissingIdPage();
 
-if (ids.length > 1) {
-  $("#page_header").append(" collection");
-  $("#cost").text(`This collection is worth ${ids.length*100}K shekels`);
 } else {
-  $("#page_header").text("Your new NFT");
+
+  firebase.database().ref(`NFT/owned/${id}`).once("value").then(snapshot => {
+    let ids = snapshot.val();
+    console.log(ids.length);
+
+    if (ids === null) {
+
+      wrongOrMissingIdPage();
+
+    } else {
+      let count = 0;
+      
+      for (id in ids) {
+        count++;
+
+        let url = `https://i.imgur.com/${id}.png`;
+        $("#nfts").append(`
+          <div id="nft">
+            <a href="#nft${id}" uk-toggle>
+              <img src="${url}" uk-img />
+            </a>
+          </div>`
+        );
+        $("#nftmodals").append(`
+          <div id="nft${id}" class="uk-flex-top" uk-modal>
+            <div class="uk-modal-dialog uk-width-auto uk-margin-auto-vertical">
+              <button class="uk-modal-close-outside" type="button" uk-close></button>
+              <img src="${url}" class="uk-height-1-2" />
+            </div>
+          </div>`
+        );
+      };
+
+      $("#page_header").append(" collection");
+      $("#cost").text(`This collection is worth ${count*10}K shekels`);
+
+    }
+
+  });
+
 }
-
-ids.forEach(id => {
-
-  let url = `https://i.imgur.com/${id}.png`;
-  $("#nfts").append(`
-    <div id="nft">
-      <a href="#nft${id}" uk-toggle>
-        <img src="${url}" uk-img />
-      </a>
-    </div>`
-  );
-  $("#nftmodals").append(`
-    <div id="nft${id}" class="uk-flex-top" uk-modal>
-      <div class="uk-modal-dialog uk-width-auto uk-margin-auto-vertical">
-        <button class="uk-modal-close-outside" type="button" uk-close></button>
-        <img src="${url}" class="uk-height-1-2" />
-      </div>
-    </div>`
-  );
-
-});
