16a82bf1d856f7b4940f7b612b03cc66bfe651b9
CNDRD <michal.mikulec.mm@gmail.com>
2023-04-02 21:10:07 +0200

    add roles

--- public/resources/js/me.js
+++ public/resources/js/me.js
@@ -4,10 +4,9 @@ if (!UUID) { window.location = "/"; }
 $('#username').text(userAuth.session.user.user_metadata.full_name);
 $('#avatar').attr('src', userAuth.session.user.user_metadata.avatar_url);
 
-//! Querying the data like this, because the IDs are apparently too long or some shit...
+//* Querying the data like this, because the IDs are apparently too long or some shit...
 const { data: userDataDb } = await supabase.from('users').select('*').like('avatar', `%/${userAuth.session.user.user_metadata.sub}/%`);
 let userData = userDataDb[0];
-c(userData);
 
 let showStat = (id, value) => { $(`#${id}.stat > .stat_value`).text(value); };
 showStat('level', addSpaces(userData.level));
@@ -15,11 +14,22 @@ showStat('xp', addSpaces(userData.xp));
 showStat('messages', addSpaces(userData.messages));
 showStat('voice', addSpaces(roundTwo(userData.total_voice / 60 / 60)));
 
+if (userData.cicina.count === 0) { $('#cicina_stats').hide(); }
 showStat('cicina_average', roundTwo(userData.cicina.average));
 showStat('cicina_tries', addSpaces(userData.cicina.count));
 showStat('cicina_longest', userData.cicina.longest);
 showStat('cicina_last', new Intl.DateTimeFormat("cs-CS").format(new Date(userData.cicina.last)));
 
-userData.roles.forEach(role => {
-    // $('#roles').append(role);
+const { data: userRoles } = await supabase.from('roles').select('name, position, color').in('id', userData.roles).order('position', {ascending: false});
+
+userRoles.forEach(role => {
+
+    $('#roles').append(`
+    
+        <div class="role" style="background-color: #${role.color}70; border-color: #${role.color};">
+            ${role.name}
+        </div>
+
+    `);
+
 });
