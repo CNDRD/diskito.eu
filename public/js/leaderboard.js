badd35955c149da175c212a7dd53ef64bd6251c8
CNDRD <michal.mikulec.mm@gmail.com>
2024-11-24 16:19:23 +0100

    + yearly voice (again)

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -35,12 +35,16 @@ ldrbrd.forEach(user => {
             <td data-what="total-voice" data-sort="${user.total_voice}">
                 ${parseVoiceTime(user.total_voice)}
             </td>
+
+            <td data-what="more-info" data-id="${user.id}">
+                <img src="icons/data_exploration.svg" />
+            </td>
         </tr>
     `);
 
 });
 
-function parseVoiceTime(seconds) {
+function parseVoiceTime(seconds, fixedWidth=false) {
     let h = Math.floor(seconds / 3600);
     let m = Math.floor((seconds % 3600) / 60);
     let s = Math.floor(seconds % 60);
@@ -50,6 +54,12 @@ function parseVoiceTime(seconds) {
     if (h == 0) { str = `${m}m ${s}s`; }
     if (h == 0 && m == 0) { str = `${s}s`; }
     if (h == 0 && m == 0 && s == 0) { str = `-`; }
+
+    if (fixedWidth) {
+        str = str.replace(/(\d+)/g, match => {
+            return match.length == 1 ? `\xa0\xa0\xa0${match}` : match;
+        });
+    }
 
     return str;
 };
@@ -84,3 +94,42 @@ $('[data-srt]').on('click', function() {
     $(this).attr('data-dir', dir == 'asc' ? 'desc' : 'asc');
 
 });
+
+
+
+$('[data-what="more-info"]').on('click', async function() { await showIndividualInfo(this); });
+
+async function showIndividualInfo(that) {
+    $(`[data-spec-row="${that.dataset.id}"]`).toggle($(`[data-spec-row="${that.dataset.id}"]`).css('display') == 'none');
+
+    $(that).find('img').remove();
+    $(that).append(spinner());
+
+    if (!$(`[data-spec-row="${that.dataset.id}"]`).length) {
+        let yearlyVoice = await supabase.from('yearly_voice').select('year, total, longest').eq('id', that.dataset.id).order('year', { ascending: false });
+        let yvData = `
+            <tr data-spec-row="${that.dataset.id}">
+                <td class="header" colspan="4">Year</td>
+                <td class="header">Total</td>
+                <td class="header">Longest</td>
+            </tr>
+        `;
+
+        yearlyVoice.data.forEach(yv => {
+            yvData += `
+                <tr data-spec-row="${that.dataset.id}">
+                    <td colspan="4">${yv.year}</td>
+                    <td>${parseVoiceTime(yv.total, true)}</td>
+                    <td>${parseVoiceTime(yv.longest, true)}</td>
+                </tr>
+            `;
+        });
+        
+        $(that).parent().after(yvData);
+    }
+
+    $(that).find('svg').remove();
+    $(that).append(`<img src="icons/data_exploration.svg" />`);
+
+    $(`[data-spec-row]:not([data-spec-row="${that.dataset.id}"])`).hide();
+};
