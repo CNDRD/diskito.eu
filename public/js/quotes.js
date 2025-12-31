709f746c42260325170434dd33446b47547d7d6e
CNDRD <michal.mikulec.mm@gmail.com>
2025-12-31 16:03:42 +0100

    Remove author filter switch from quotes page
    Eliminated the 'f_switch' author filter UI and its related styles and JavaScript logic from the quotes page. This simplifies the interface by removing the author-based filtering functionality.

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -2,11 +2,7 @@ import { c, supabase, spinner } from './main.js';
 
 const { data: quotes } = await supabase.from('quotes').select('*').order('created_at', { ascending: false });
 
-let uniqueAuthors = {};
-
 quotes.forEach(quote => {
-    uniqueAuthors[quote.by] = uniqueAuthors[quote.by]+1 || 1;
-
     let authorName = quote.by;
     authorName = authorName.replace('-', '_');
 
@@ -23,32 +19,4 @@ quotes.forEach(quote => {
     
     `);
 
-});
-
-// order the authors by their count
-Object.entries(uniqueAuthors).sort((a, b) => b[1] - a[1]).forEach(authorSet => {
-    let author, count;
-    [author, count] = authorSet;
-
-    let authorId = author.replace('-', '_');
-
-    $('#peeps_switch').append(`
-        <input type="radio" name="quote_peep" id="${authorId}" />
-        <label for="${authorId}" class="peep">
-            ${author}
-            <span class="count">${count}</span>
-        </label>
-    `);
-});
-
-$('#peeps_switch > input').on('change', function() {
-    let author = $(this).attr('id');
-    $('#quotes > .quote').each(function() {
-        let quoteAuthor = $(this).data('author');
-        if (quoteAuthor == author) {
-            $(this).show();
-        } else {
-            $(this).hide();
-        }
-    });
 });
